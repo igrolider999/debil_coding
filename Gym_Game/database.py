f7bd40781b1130c3669356989d33b96bb4704c92
igrolider <igrolider321@gmail.com>
2025-07-01 18:32:23 +0200

    govnooo

--- Gym_Game/database.py
+++ Gym_Game/database.py
@@ -7,14 +7,14 @@ cursor = connection.cursor()
 cursor.execute("""CREATE TABLE IF NOT EXISTS progress(hp INTEGER, bucks INTEGER, damage INTEGER)""")
 
 
-# cursor.execute("INSERT INTO progress VALUES (90, 1400, 10)")
+cursor.execute("INSERT INTO progress VALUES (90, 1400, 10)")
 
 cursor.execute("SELECT * FROM progress")
 
 result = cursor.fetchall()
 print(result)
 
-cursor.execute("""DELETE FROM progress WHERE hp = 90""")
+# cursor.execute("""DELETE FROM progress WHERE hp = 90""")
 
 connection.commit()
 connection.close()
